dd6a4612a2b6ce8bf5494ff15874e031b11e8f87
Naalfioiflaan <alifianadira11@gmail.com>
2023-05-05 09:05:11 +0700

    tampilan halaman klasifikasi berita admin done

--- kelola_pengguna/views.py
+++ kelola_pengguna/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def kelola_pengguna(request):
     context = {
-        'nama' : 'Kelola Pengguna'
+        'nama' : 'Kelola Pengguna',
+        'user' : 'Nadira Alifia Ionendri',
+        'email_user' : 'alifianadira11@gmail.com'
     }
     return render(request, 'kelola_pengguna/kelola_pengguna.html', context)
--- klasifikasi_berita/views.py
+++ klasifikasi_berita/views.py
@@ -4,5 +4,8 @@ from django.shortcuts import render
 def klasifikasi_berita(request):
     context = {
         'nama' : 'Klasifikasi Berita',
+        'user' : 'Nadira Alifia Ionendri',
+        'email_user' : 'alifianadira11@gmail.com',
+        'tgl_terakhir_diambil' : '4 Mei 2023',
     }
     return render(request, 'klasifikasi_berita/klasifikasi_berita.html', context)
