c1df3cd5d92c674b9585a4a3489be11dfd6374a6
Naalfioiflaan <alifianadira11@gmail.com>
2023-05-10 07:35:31 +0700

    menyambungkan dan membuat database

--- webapp/settings.py
+++ webapp/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'spb',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
+        'USER' : 'root',
+        'PASSWORD' : '',
     }
 }
 
